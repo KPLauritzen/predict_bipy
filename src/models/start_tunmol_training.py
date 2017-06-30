19af73e7bc7771813a4fb11d0d7a86c0c7a6cac0
Kasper Primdal Lauritzen <kplauritzen@gmail.com>
2017-06-30 12:52:33 +0200

    tunmol frac training data

--- src/models/start_tunmol_training.py
+++ src/models/start_tunmol_training.py
@@ -9,14 +9,14 @@ script_path = os.path.abspath('src/models/train_predict.py')
 
 
 
-n_nodes = [4, 6, 8, 10]
+n_nodes = [6, 10]
 dense = ['--extra_dense', '']
 recurrent_units = ['lstm', 'gru']
-upper_lower_cutoffs = [(1e-1, 1e-7), (1e-1, 1e-6)]
+upper_lower_cutoffs = [(1e-1, 1e-7)]
 seeds = [1, 2, 3]
+training_used = np.linspace(0.1, 1.0, num=4)
 
-
-iterables = (n_nodes, dense, recurrent_units, upper_lower_cutoffs, seeds)
+iterables = (n_nodes, dense, recurrent_units, upper_lower_cutoffs, seeds, training_used)
 total_length = np.prod([len(x) for x in iterables])
 print "You are about to submit {} jobs.".format(total_length)
 var = raw_input("Is this OK? [y/n]: ")
@@ -27,10 +27,10 @@ else:
     exit()
 
 
-for  nodes, extra_dense, recur_unit, upper_lower, seed in itertools.product(*iterables):
+for  nodes, extra_dense, recur_unit, upper_lower, seed, train_data_used in itertools.product(*iterables):
     upper_cut, lower_cut = upper_lower
     datafile_basename = 'open_17_03_31_BP_4K_{}.dat'
     jobname = 'bipy_tunmol__{}__{}nodes'.format(recur_unit, nodes)
-    submit_string = """python {submit_path} --scriptname={script_path} --partition kemi_gemma3 --no_scratch --mail fail --jobname {jobname} --jobid --py_args='--do_train --do_predict --n_nodes {nodes} --n_epochs 300 {extra_dense} --recurrent_unit {recur_unit} --upper_cutoff {upper_cut} --lower_cutoff {lower_cut} --datadir data/processed/4K_traces --datafile_basename {datafile_basename} --modeldir models/tun-mol-seed-{seed} --pos_idx_file data/processed/molecular_index.csv --neg_idx_file data/processed/tunnel_index.csv --predict_idx_file data/processed/all_index.csv --seed {seed}'""".format(**locals())
+    submit_string = """python {submit_path} --scriptname={script_path} --partition kemi_gemma3 --no_scratch --mail fail --jobname {jobname} --jobid --py_args='--do_train --do_predict --n_nodes {nodes} --n_epochs 300 {extra_dense} --recurrent_unit {recur_unit} --upper_cutoff {upper_cut} --lower_cutoff {lower_cut} --datadir data/processed/4K_traces --datafile_basename {datafile_basename} --modeldir models/tun-mol-seed-{seed}-frac-train-{train_data_used} --pos_idx_file data/processed/molecular_index.csv --neg_idx_file data/processed/tunnel_index.csv --predict_idx_file data/processed/all_index.csv --seed {seed} --fraction_training_data_used {train_data_used}'""".format(**locals())
     os.system(submit_string)
     os.system("sbatch submit.sl")
